9194717ca5286a4652ff0bd888729bd2183ec7e3
Laura Trager <laura.trager@gmail.com>
2017-10-27 18:01:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,10 +17,13 @@ end
 #rooms = [1, 2, 3, 4, 5, 6, 7]
 
 def assign_rooms(speakers)
+  speaker_with_room = []
   speakers.each_with_index do |name, index| #I've stored the name and index
     room_number = index + 1
+    speaker_with_room << "#{name} #{room_number}"
     puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
   end
+  speaker_with_room
 end
 
 
