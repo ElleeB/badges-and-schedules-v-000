34bdf5514eeb7bfa798002bb0ff468790ac80125
Laura Trager <laura.trager@gmail.com>
2017-10-27 16:58:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
   speakers.collect do |name|
     puts "Hello, my name is #{name}."
   end
 end
-    
