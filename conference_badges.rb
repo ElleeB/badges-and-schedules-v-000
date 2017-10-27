ca309aa350246b6aeb14b9a6ecc12ecb36261c48
Laura Trager <laura.trager@gmail.com>
2017-10-27 18:31:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,11 @@ def batch_badge_creator(speakers)
   end
 end
 
+# => ["Hello, my name is Edsger.", "Hello, my name is Ada.", "Hello, my
+#    name is Charles.", "Hello, my name is Alan.", "Hello, my name is Grace.",
+#   "Hello, my name is Linus.", "Hello, my name is Matz."]
+
+
 def assign_rooms(speakers)
   speaker_with_room = []
   speakers.each_with_index do |name, index|
@@ -19,9 +24,15 @@ def assign_rooms(speakers)
   speaker_with_room
 end
 
+#  ["Hello, Edsger! You'll be assigned to room 1!", "Hello, Ada! You'll
+#  be assigned to room 2!", "Hello, Charles! You'll be assigned to room 3!",
+# "Hello, Alan! You'll be assigned to room 4!", "Hello, Grace! You'll
+#  be assigned to room 5!", "Hello, Linus! You'll be assigned to room 6!", "
+#  Hello, Matz! You'll be assigned to room 7!"]
+
+
 def printer
-  puts batch_badge_creator(speakers)
-  puts assign_rooms(speakers)
+
 end
 
 
