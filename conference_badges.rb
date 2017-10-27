b146b2ff7a147bf7a39a577e7bbaffd516454b5a
Laura Trager <laura.trager@gmail.com>
2017-10-27 16:54:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 
 
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+#speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
 
 def badge_maker(name)
   return "Hello, my name is #{name}"
