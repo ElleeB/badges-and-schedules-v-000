80122fe7027e64358d68bc348c9b29278a8f94ba
Laura Trager <laura.trager@gmail.com>
2017-10-27 18:38:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -30,7 +30,7 @@ end
 #  Hello, Matz! You'll be assigned to room 7!"]
 
 def printer(speakers)
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  #speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   batch_badge_creator(speakers).each do |badge|
     puts badge
   end
